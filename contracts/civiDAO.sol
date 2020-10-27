1be2fa672fdf9140a497745e2e27f06869eb6b2f
hooftly <mhooft@tf-dev.org>
2020-10-27 06:57:01 -0600

    Basic skeleton to add Proposals
    Users Will need to submit a website url that will be converted and stored as Bytes32
    
    Proposals will likely be instructed to use a URL Shortener.

--- contracts/civiDAO.sol
+++ contracts/civiDAO.sol
@@ -6,27 +6,49 @@ import "./SafeMath.sol";
 
 contract civiDAO is AccessControl {
 
-using SafeMath for uint256;
+    using SafeMath for uint256;
 
-IERC20 private fundamenta;
+    IERC20 private fundamenta;
 
-//----------RBAC-------------------
+    //----------RBAC-------------------
 
-bytes32 public constant _ADMIN = keccak256("_ADMIN");
+    bytes32 public constant _ADMIN = keccak256("_ADMIN");
+    bytes32 public constant WEBSITE = keccak256("fundamenta.network"); // TEST STRING 
 
-struct Proposals {
-    address Originator;
-    bytes32 Website;
-    uint BlockHeightProposed;
-    uint AllocatedWeight;
-    bool Active;
-}
+    struct Proposals {
+        address Originator;
+        bytes32 Website;
+        uint BlockHeightProposed;
+        uint AllocatedWeight;
+        bool Active;
+        uint ProposalID;
+    }
 
-//------Set Token Address----------
+    Proposals[] public proposalArray;
 
-function setAddress(IERC20 _token) public {
-    require(hasRole(_ADMIN, msg.sender));
-    fundamenta = _token;
-}
+    //------Set Token Address-----------
+
+    function setAddress(IERC20 _token) public {
+        require(hasRole(_ADMIN, msg.sender));
+        fundamenta = _token;
+    }
+
+    //-------DAO Fundtions--------------
+
+    function addProposal(bytes32 _website) public {
+        uint propid = propID().add(1);
+        proposalArray.push(Proposals({
+            Originator: msg.sender,
+            Website: _website,
+            BlockHeightProposed: block.number,
+            AllocatedWeight: 0,
+            Active: false,
+            ProposalID: propid
+        }));
+    }
+
+    function propID() public view returns (uint) {
+        return proposalArray.length;
+    }
 
 }
