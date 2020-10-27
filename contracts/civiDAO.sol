3d522b172a51997d7613b03ce5dc8fea062e4925
hooftly <mhooft@tf-dev.org>
2020-10-27 05:33:24 -0600

    Set Token
    - Add Token interface (IERC20)
    - Add function that requires the `_ADMIN` role to set FMTA Token Address

--- contracts/civiDAO.sol
+++ contracts/civiDAO.sol
@@ -8,8 +8,17 @@ contract civiDAO is AccessControl {
 
 using SafeMath for uint256;
 
+IERC20 private fundamenta;
+
 //----------RBAC-------------------
 
 bytes32 public constant _ADMIN = keccak256("_ADMIN");
+
+//------Set Token Address----------
+
+function setAddress(IERC20 _token) public {
+    require(hasRole(_ADMIN, msg.sender));
+    fundamenta = _token;
+}
 
 }
