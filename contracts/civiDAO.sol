852c0b1c9baaa6fbb0a06a2230ccae21549d6d3d
hooftly <mhooft@tf-dev.org>
2020-10-27 05:13:34 -0600

    Multiple Changes
    - import SafeERC20.sol and SafeMath.sol to civiDAO
    - use SafeMath for uint256

---
+++ contracts/civiDAO.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.7.0;
+
+import "./AccessControl.sol";
+import "./SafeERC20.sol";
+import "./SafeMath.sol";
+
+contract civiDAO is AccessControl {
+
+using SafeMath for uint256;
+
+}
